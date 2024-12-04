3db9d7fbe7be0e453b2c2175701dd7b4f13fb1cd
Chuck Norris <me@example.com>
2024-12-04 11:06:51 +0100

    Added level editor skeleton

---
+++ constatnts.py
@@ -0,0 +1,6 @@
+AIR = 0
+WALL = 1
+BARREL = 2
+
+NAMES = ['air', 'wall', 'barrel']
+VALUES = dict(zip(NAMES, range(len(NAMES))))
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+import constatnts
 
-class RangePicker:
-    def __init__(self, master, label_text, min, max):
+class Subprogram:
+    def __init__(self, master):
         self.frame = tk.Frame(master=master)
+
+class RangePicker(Subprogram):
+    def __init__(self, master, label_text, min, max):
+        super().__init__(master)
         self.min = min
         self.max = max
         self.left_btn = tk.Button(master=self.frame, text='-', command=lambda: self.add(-1))
@@ -22,6 +27,32 @@ class RangePicker:
             self.count += x
             self.count_label.configure(text=f'{self.count}')
 
+class LevelEditor(Subprogram):
+    def __init__(self, master, size, blocksize):
+        super().__init__(master)
+        self.blocksize = blocksize
+        self.size = size
+        self.canvas = tk.Canvas(master=self.frame, width=size, height=size)
+        self.toolbar = tk.Frame(master=self.frame)
+        self.load_btn = tk.Button(master=self.toolbar, text='Load Map')
+        self.save_btn = tk.Button(master=self.toolbar, text='Save Map')
+        self.placing_frame = tk.Frame(master=self.frame)
+        combo_label = tk.Label(master=self.placing_frame, text='Placing: ')
+        self.placing = tk.StringVar()
+        self.placing_combo = ttk.Combobox(master=self.placing_frame, values=constatnts.NAMES, textvariable=self.placing, state='readonly')
+        self.placing_combo.current(0)
+        self.placing_combo.bind('<<ComboboxSelected>>', lambda x: print(x, self.placing.get()))
+
+        self.toolbar.grid(row=0, column=0, sticky='w')
+        self.load_btn.pack(side=tk.LEFT)
+        self.save_btn.pack(side=tk.LEFT)
+        self.canvas.grid(row=1, column=0)
+        self.placing_frame.grid(row=2, column=0)
+        combo_label.pack(side=tk.LEFT)
+        self.placing_combo.pack(side=tk.LEFT)
+
+
+
 
 class Program:
     def __init__(self):
@@ -29,7 +60,8 @@ class Program:
         
         self.menu_frame = tk.Frame(master=self.window)
         self.menu_btn_play = tk.Button(master=self.menu_frame, text='Play', font='Helvetica 32', command=self.from_menu_to_level_select)
-        self.menu_btn_editor = tk.Button(master=self.menu_frame,text='Level Editor', font='Helvetica 32')
+        self.menu_btn_editor = tk.Button(master=self.menu_frame,text='Level Editor', font='Helvetica 32', command=self.from_menu_to_editor)
+
         self.menu_btn_play.grid(row=0, column=0, sticky='nswe')
         self.menu_btn_editor.grid(row=1, column=0, sticky='nswe')
 
@@ -53,6 +85,8 @@ class Program:
         self.level_selector_play_btn.grid(row=3, column=1, sticky='nswe')
 
 
+        self.level_editor = LevelEditor(self.window, 800, 20)
+
 
         self.menu_frame.pack()
 
@@ -66,6 +100,10 @@ class Program:
     def from_level_select_to_menu(self):
         self.level_selector_frame.pack_forget()
         self.menu_frame.pack()
+
+    def from_menu_to_editor(self):
+        self.menu_frame.pack_forget()
+        self.level_editor.frame.pack()
 
 if __name__ == '__main__':
     p = Program()
