1f633314980acee6bedb6600f56a87fe130d2fa3
Chuck Norris <me@example.com>
2024-12-07 20:14:42 +0100

    Added min length parameter to split

--- spritesheeter.py
+++ spritesheeter.py
@@ -1,45 +1,66 @@
 from PIL import Image
 import numpy as np
 
-def split_vertically(data, alpha_threshold=0):
-    start = 0
+def split_vertically(data, alpha_threshold=0, min_length=0):
+    start = -1
+    end = -1
     splits = []
     for x in range(data.shape[1]):
         if np.all(data[:,x,3] <= alpha_threshold):
-            if start < x:
-                splits.append(data[:,start:x,:])
-            start = x+1
+            if x-start >= min_length and start != -1:
+                splits.append(data[:,start:end+1,:])
+                start = -1
+                end = -1
+        else:
+            if start == -1:
+                start = x
+            end = x
 
-    if start < x:
-        splits.append(data[:,start:x,:])
+    if start < end and end-start >= min_length:
+        splits.append(data[:,start:end+1,:])
     
     return splits
 
-def split_horizontally(data, alpha_threshold=0):
+def split_horizontally(data, alpha_threshold=0, min_length=0):
     splits = []
-    start = 0
+    start = -1
+    end = -1
     for y in range(data.shape[0]):
         if np.all(data[y,:,3] <= alpha_threshold):
-            if start < y:
-                splits.append(data[start:y,:,:])
-            start = y+1
+            if y-start >= min_length and start != -1:
+                print(start,end)
+                splits.append(data[start:end+1,:,:])
+                start = -1
+                end = -1
+        else:
+            if start == -1:
+                start = y
+            end = y
 
-    if start < y:
-        splits.append(data[start:y,:,:])
+    if start < end and end-start >= min_length:
+        splits.append(data[start:end+1,:,:])
     
     return splits
 
 
-def split(filepath, alpha_threshold=0):
+def split(filepath, alpha_threshold=0, min_length=0):
     image = Image.open(filepath).convert('RGBA')
     # print(image.mode)
     data = np.array(image)
     # print(data.shape)
 
-    horizontal = split_horizontally(data, alpha_threshold)
+    horizontal = split_horizontally(data, alpha_threshold, min_length)
+
+    # for x in horizontal:
+        # Image.fromarray(x).show()
+        # print(x)
+        # pass
+    # print(horizontal[-1], np.nonzero(horizontal[-1]), horizontal[-1][0,256])
+    # return
+
     splits = []
     for x in horizontal:
-        splits.append(split_vertically(x, alpha_threshold))
+        splits.append(split_vertically(x, alpha_threshold, min_length))
 
     # print(len(splits))
     for i in range(len(splits)):
@@ -48,12 +69,12 @@ def split(filepath, alpha_threshold=0):
     
     return splits
 
-def split_and_save(filepath, alpha_threshold=0):
-    splits = split(filepath, alpha_threshold)
+def split_and_save(filepath, alpha_threshold=0, min_length=0):
+    splits = split(filepath, alpha_threshold, min_length)
     
     for i in range(len(splits)):
         for j in range(len(splits[i])):
             splits[i][j].save(f'computed/{filepath.split('/')[-1].split('.')[0]}_{i}_{j}.png')    
             
 if __name__ == '__main__':
-    split_and_save('assets/bomb.png', 50)
+    split_and_save('assets/player.png', 0, 0)
