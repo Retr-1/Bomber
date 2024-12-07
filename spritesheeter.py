dd1161c02ba475f07f8440e845c63a7b1105b5a7
Chuck Norris <me@example.com>
2024-12-07 19:38:17 +0100

    Added alpha threshold to spritesheeter

--- spritesheeter.py
+++ spritesheeter.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import numpy as np
 
-def split_vertically(data):
+def split_vertically(data, alpha_threshold=0):
     start = 0
     splits = []
     for x in range(data.shape[1]):
-        if np.all(data[:,x,3] == 0):
+        if np.all(data[:,x,3] <= alpha_threshold):
             if start < x:
                 splits.append(data[:,start:x,:])
             start = x+1
@@ -15,11 +15,11 @@ def split_vertically(data):
     
     return splits
 
-def split_horizontally(data):
+def split_horizontally(data, alpha_threshold=0):
     splits = []
     start = 0
     for y in range(data.shape[0]):
-        if np.all(data[y,:,3] == 0):
+        if np.all(data[y,:,3] <= alpha_threshold):
             if start < y:
                 splits.append(data[start:y,:,:])
             start = y+1
@@ -29,32 +29,17 @@ def split_horizontally(data):
     
     return splits
 
-def split_and_save(filepath):
+
+def split(filepath, alpha_threshold=0):
     image = Image.open(filepath).convert('RGBA')
     # print(image.mode)
     data = np.array(image)
     # print(data.shape)
 
-    horizontal = split_horizontally(data)
+    horizontal = split_horizontally(data, alpha_threshold)
     splits = []
     for x in horizontal:
-        splits.append(split_vertically(x))
-
-    # print(len(splits))
-    for i in range(len(splits)):
-        for j in range(len(splits[i])):
-            Image.fromarray(splits[i][j]).save(f'computed/{filepath.split('/')[-1].split('.')[0]}_{i}_{j}.png')
-
-def split(filepath):
-    image = Image.open(filepath).convert('RGBA')
-    # print(image.mode)
-    data = np.array(image)
-    # print(data.shape)
-
-    horizontal = split_horizontally(data)
-    splits = []
-    for x in horizontal:
-        splits.append(split_vertically(x))
+        splits.append(split_vertically(x, alpha_threshold))
 
     # print(len(splits))
     for i in range(len(splits)):
@@ -63,7 +48,12 @@ def split(filepath):
     
     return splits
 
+def split_and_save(filepath, alpha_threshold=0):
+    splits = split(filepath, alpha_threshold)
     
+    for i in range(len(splits)):
+        for j in range(len(splits[i])):
+            splits[i][j].save(f'computed/{filepath.split('/')[-1].split('.')[0]}_{i}_{j}.png')    
             
 if __name__ == '__main__':
-    split('assets/player.png')
+    split_and_save('assets/bomb.png', 50)
